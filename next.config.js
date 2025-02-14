4cabc9fdcb278a4ae329cd4f215f183877ea423e
hojuncha <hojun.cha997@gmail.com>
2025-02-14 21:46:59 +0900

    fix: 외부 이미지 도메인 사용 에러 해결
    :next.config.js에서 수파베이스 스토리지 경로 추가해줌으로서 외부 이미지 허용

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: process.env.NEXT_PUBLIC_SUPABASE_URL?.replace('https://', ''),
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+}
+
+module.exports = nextConfig 
