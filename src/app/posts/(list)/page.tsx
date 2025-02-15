38bf758ef17f0340c5b32145a5eb568629c0d416
hojuncha <hojun.cha997@gmail.com>
2025-02-15 23:24:23 +0900

    chore: 스켈레톤 추가

--- src/app/posts/(list)/page.tsx
+++ src/app/posts/(list)/page.tsx
@@ -11,6 +11,8 @@ import { useState, useEffect } from 'react';
 import { ChevronDown } from 'lucide-react';
 import { extractTextFromContent } from '@/utils/postUtils'
 import { PostForList, PostListResponse } from '@/types/posts/postTypes';
+import PostListSkeleton from '@/components/posts/skeletons/PostListSkeleton';
+import Image from 'next/image';
 
 export default function Post() {
     const router = useRouter();
@@ -122,8 +124,16 @@ export default function Post() {
     console.log('로딩 상태:', isLoading);
     console.log('에러:', error);
     
+    if (isLoading) {
+        return <PostListSkeleton />;
+    }
+
+    if (!data) {
+        return <div className="text-center py-8">데이터를 불러올 수 없습니다.</div>;
+    }
+
     return (
-        <div className="w-full ">
+        <div className="w-full">
             {/* <h1 className="text-3xl font-bold mb-12 border-b border-black pb-4">포스팅</h1> */}
             
             {/* 아코디언 필터 영역 */}
@@ -209,99 +219,92 @@ export default function Post() {
             </div>
 
             <div className="w-full">
-                {isLoading ? (
-                    <div className="text-center py-8">로딩중...</div>
-                ) : !data ? (
-                    <div className="text-center py-8">데이터를 불러올 수 없습니다.</div>
-                ) : (
-                    <div>
-                        <div className='bg-white rounded-lg overflow-hidden'>
-                            {data.data.length === 0 ? (
-                                <div className='text-center py-20 text-gray-500'>
-                                    <p className='text-lg font-medium'>등록된 포스팅이 없습니다.</p>
-                                </div>
-                            ) : (
-                                data.data.map((post: PostForList) => (
-                                    <div 
-                                        key={post.public_id} 
-                                        className='border-b border-black cursor-pointer py-4 group'
-                                    >
-                                        <div className='flex items-center'>
-                                            <div className='flex-1'>
-                                                <Link 
-                                                    href={`/posts/${post.slug}-${post.public_id}`}
-                                                    className='block'
-                                                >
-                                                    <div className='text-lg font-medium text-black group-hover:text-blue-500 flex items-center gap-2 mb-2'>
-                                                        {post.isSecret && (
-                                                            <span className="text-xs bg-gray-200 px-2 py-0.5 rounded">비밀글</span>
-                                                        )}
-                                                        {post.title}
-                                                    </div>
-                                                    {/* 본문 미리보기 */}
-                                                    {post.excerpt && (
-                                                        <div className='text-sm text-gray-600 mb-2 line-clamp-2'>
-                                                            {post.excerpt}
-                                                        </div>
-                                                    )}
-                                                    <div className='text-sm text-gray-500 flex flex-wrap gap-4'>
-                                                        <span>{post.author_display_name.includes('@') ? post.author_display_name.split('@')[0] : post.author_display_name}</span>
-                                                        <span>조회 {post.viewCount}</span>
-                                                        <span>좋아요 {post.likeCount}</span>
-                                                        <span>{format(new Date(post.createdAt), 'yyyy-MM-dd')}</span>
-                                                    </div>
-                                                </Link>
+                <div>
+                    <div className='bg-white rounded-lg overflow-hidden'>
+                        {data.data.length === 0 ? (
+                            <div className='text-center py-20 text-gray-500'>
+                                <p className='text-lg font-medium'>등록된 포스팅이 없습니다.</p>
+                            </div>
+                        ) : (
+                            data.data.map((post: PostForList) => (
+                                <Link 
+                                    key={post.public_id}
+                                    href={`/posts/${post.slug}-${post.public_id}`}
+                                    className='block border-b border-b-gray-300 cursor-pointer py-4 group'
+                                >
+                                    <div className='flex items-center'>
+                                        <div className='flex-1'>
+                                            <div className='text-lg font-medium text-black group-hover:text-blue-500 flex items-center gap-2 mb-2'>
+                                                {post.isSecret && (
+                                                    <span className="text-xs bg-gray-200 px-2 py-0.5 rounded">비밀글</span>
+                                                )}
+                                                {post.title}
                                             </div>
-                                            <div className='w-40 ml-4'>
-                                                {/* 썸네일 영역 */}
-                                                <div className={`w-full h-24 rounded-lg overflow-hidden ${!post.thumbnail ? 'bg-gradient-to-r from-blue-500 to-purple-500 flex items-center justify-center p-2' : ''}`}>
-                                                    {post.thumbnail ? (
-                                                        <img 
-                                                            src={post.thumbnail} 
-                                                            alt={post.title}
-                                                            className="w-full h-full object-cover"
-                                                        />
-                                                    ) : (
-                                                        <span className="text-white text-sm font-medium text-center line-clamp-2">
-                                                            {post.title}
-                                                        </span>
-                                                    )}
+                                            {/* 본문 미리보기 */}
+                                            {post.excerpt && (
+                                                <div className='text-sm text-gray-600 mb-2 line-clamp-2'>
+                                                    {post.excerpt}
                                                 </div>
+                                            )}
+                                            <div className='text-sm text-gray-500 flex flex-wrap gap-4'>
+                                                <span className='px-2 py-1 bg-gray-100 text-sm rounded-md'>{post.category}</span>
+                                                <span>{post.author_display_name.includes('@') ? post.author_display_name.split('@')[0] : post.author_display_name}</span>
+                                                <span>조회 {post.viewCount}</span>
+                                                <span>좋아요 {post.likeCount}</span>
+                                                <span>{format(new Date(post.createdAt), 'yyyy-MM-dd')}</span>
+                                            </div>
+                                        </div>
+                                        <div className='w-40 ml-4'>
+                                            {/* 썸네일 영역 */}
+                                            <div className={`w-full h-24 rounded-lg overflow-hidden ${!post.thumbnail ? 'bg-gradient-to-r from-blue-500 to-purple-500 flex items-center justify-center p-2' : ''}`}>
+                                                {post.thumbnail ? (
+                                                    <Image 
+                                                        src={post.thumbnail} 
+                                                        alt={post.title}
+                                                        width={100}
+                                                        height={100}
+                                                        className="w-full h-full object-cover"
+                                                    />
+                                                ) : (
+                                                    <span className="text-white text-sm font-medium text-center line-clamp-2">
+                                                        {post.title}
+                                                    </span>
+                                                )}
                                             </div>
                                         </div>
                                     </div>
-                                ))
-                            )}
-                        </div>
-
-                        {/* 페이지네이션 */}
-                        {data.meta && (
-                            <div className="mt-6 flex justify-center gap-2">
-                                {Array.from({ length: data.meta.totalPages }, (_, i) => i + 1).map((page) => (
-                                    <button
-                                        key={page}
-                                        onClick={() => handlePageChange(page)}
-                                        className={`px-4 py-2 rounded-md ${
-                                            searchState.page === page 
-                                                ? 'bg-blue-500 text-white' 
-                                                : 'bg-gray-100 hover:bg-gray-200'
-                                        }`}
-                                    >
-                                        {page}
-                                    </button>
-                                ))}
-                            </div>
+                                </Link>
+                            ))
                         )}
-
-                        <div className="mt-4 flex justify-end">
-                            <Link href="/posts/write">
-                                <button className="px-4 py-2 bg-black text-white rounded-md hover:bg-blue-600 transition-colors">
-                                    게시글 작성
-                                </button>
-                            </Link>
-                        </div>
                     </div>
-                )}
+
+                    {/* 페이지네이션 */}
+                    {data.meta && (
+                        <div className="mt-6 flex justify-center gap-2">
+                            {Array.from({ length: data.meta.totalPages }, (_, i) => i + 1).map((page) => (
+                                <button
+                                    key={page}
+                                    onClick={() => handlePageChange(page)}
+                                    className={`px-4 py-2 rounded-md ${
+                                        searchState.page === page 
+                                            ? 'bg-blue-500 text-white' 
+                                            : 'bg-gray-100 hover:bg-gray-200'
+                                    }`}
+                                >
+                                    {page}
+                                </button>
+                            ))}
+                        </div>
+                    )}
+
+                    <div className="mt-4 flex justify-end">
+                        <Link href="/posts/write">
+                            <button className="px-4 py-2 bg-black text-white rounded-md hover:bg-blue-600 transition-colors">
+                                게시글 작성
+                            </button>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/app/posts/[slugAndId]/page.tsx
+++ src/app/posts/[slugAndId]/page.tsx
@@ -11,6 +11,7 @@ import Tiptap from '@/components/editor/tiptap/Tiptap';
 import { notFound } from 'next/navigation';
 import { PostNavigation } from '@/components/posts/PostNavigation';
 import { RelatedPosts } from '@/components/posts/RelatedPosts';
+import PostDetailSkeleton from '@/components/posts/skeletons/PostDetailSkeleton';
 
 interface Comment {
     id: string;
@@ -34,7 +35,7 @@ const PostDetail: React.FC = () => {
     const [replyContent, setReplyContent] = useState('');
 
     if (isLoading) {
-        return <div>로딩 중...</div>;
+        return <PostDetailSkeleton />;
     }
 
     if (error || !post) {
@@ -49,16 +50,16 @@ const PostDetail: React.FC = () => {
                 <div className="max-w-4xl w-full mx-auto py-4 sm:py-8">
                     <div className="w-full space-y-6">
                         {/* 본문 컨테이너 */}
-                        <div className="bg-white shadow-md">
-                            <div className="p-4 sm:p-6">
+                        <div className="bg-white  rounded-lg sm: border-none">
+                            <div className="p-0 sm:p-6">
                                 {/* 헤더 */}
-                                <div className="border-b border-gray-200 pb-4">
+                                <div className="border-b border-gray-300 pb-4">
                                     <div className="flex items-center gap-2 mb-2">
-                                        <span className="px-2 py-1 bg-gray-100 text-sm rounded-md">{post.category}</span>
                                         <h1 className="text-2xl font-bold">{post.title}</h1>
                                     </div>
                                     <div className="flex gap-4 text-sm text-gray-600">
-                                        <span>작성자: {post.author_display_name}</span>
+                                        <span className="px-2 py-1 bg-gray-100 text-sm rounded-md">{post.category}</span>
+                                        <span>작성자: {post.author_display_name.includes('@') ? post.author_display_name.split('@')[0] : post.author_display_name}</span>
                                         <span>조회수: {post.viewCount}</span>
                                         <span>작성일: {format(new Date(post.createdAt), 'yyyy-MM-dd')}</span>
                                     </div>
@@ -75,7 +76,7 @@ const PostDetail: React.FC = () => {
                                 </div>
 
                                 {/* 좋아요 버튼 */}
-                                <div className="mt-8 border-t pt-4">
+                                <div className="mt-8 border-t border-gray-300 pt-4">
                                     <div className="flex justify-center mb-8">
                                         <button 
                                             className={`flex items-center gap-2 px-4 py-2 rounded-full transition-colors
@@ -86,27 +87,33 @@ const PostDetail: React.FC = () => {
                                         </button>
                                     </div>
                                 </div>
+
+                                <div className="p-4 sm:p-6 border border-gray-300 rounded-lg">
+                                    <div className="text-gray-500 text-center py-4">
+                                        댓글 기능 준비 중...
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
                         {/* 댓글 섹션 */}
-                        <div className="bg-white shadow-md">
+                        {/* <div className="bg-white shadow-md">
                             <div className="p-4 sm:p-6">
                                 <div className="text-gray-500 text-center py-4">
                                     댓글 기능 준비 중...
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
 
                         {/* 관련 포스트 섹션 */}
-                        <div className="bg-white shadow-md">
+                        <div className="bg-white border border-gray-300 rounded-lg">
                             <div className="p-4 sm:p-6">
                                 <RelatedPosts publicId={publicId} />
                             </div>
                         </div>
 
                         {/* 네비게이션 섹션 */}
-                        <div className="bg-white shadow-md">
+                        <div className="bg-white border border-gray-300 rounded-lg">
                             <div className="p-4 sm:p-6">
                                 <PostNavigation publicId={publicId} />
                             </div>
