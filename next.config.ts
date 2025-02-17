dcbbd9b6db2c0f2b7a531a6a3e98b5e3ee01945e
hojuncha <hojun.cha997@gmail.com>
2025-02-17 19:06:22 +0900

    feat: next.js 설정파일 설정
    : 외부 이미지 경로 허용

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: process.env.NEXT_PUBLIC_SUPABASE_URL?.replace('https://', '') || '',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
