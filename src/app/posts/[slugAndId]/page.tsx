44d1735d4e27760980aa2e235148781780d84f32
hojuncha <hojun.cha997@gmail.com>
2025-02-17 23:42:12 +0900

    fix: 서버 컴포넌트에 async-awiat 적용(에러 수정)

--- src/app/posts/[slugAndId]/page.tsx
+++ src/app/posts/[slugAndId]/page.tsx
@@ -1,3 +1,4 @@
+// src/app/posts/[slugAndId]/page.tsx
 import PostDetailClient from './PostDetailClient';
 import { Suspense } from 'react';
 import PostDetailSkeleton from '@/components/posts/skeletons/PostDetailSkeleton';
@@ -6,8 +7,10 @@ interface PageProps {
     params: { slugAndId: string };
 }
 
-export default function PostPage({ params }: PageProps) {
-    const publicId = params.slugAndId.split('-').pop() || '';
+export default async function PostPage({ params }: PageProps) {
+    // params 객체 자체를 await. 속성을 await 처리하는 것이 아니라 객체 자체를 await 처리
+    const resolvedParams = await params;
+    const publicId = resolvedParams.slugAndId.split('-').pop() || '';
 
     return (
         <Suspense fallback={<PostDetailSkeleton />}>
