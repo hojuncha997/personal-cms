eae8752c88810be9541e3be1d09d92d562cf6ea9
hojuncha <hojun.cha997@gmail.com>
2025-03-05 20:23:17 +0900

    chore: 빌드 테스트

--- src/app/posts/[slugAndId]/page.tsx
+++ src/app/posts/[slugAndId]/page.tsx
@@ -4,13 +4,14 @@ import { Suspense } from 'react';
 import PostDetailSkeleton from '@/components/posts/skeletons/PostDetailSkeleton';
 
 interface PageProps {
-    params: { slugAndId: string };
+    params: {
+        slugAndId: string;
+    };
+    searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default async function PostPage({ params }: PageProps) {
-    // params 객체 자체를 await. 속성을 await 처리하는 것이 아니라 객체 자체를 await 처리
-    const resolvedParams = await params;
-    const publicId = resolvedParams.slugAndId.split('-').pop() || '';
+export default function PostPage({ params, searchParams }: PageProps) {
+    const publicId = params.slugAndId.split('-').pop() || '';
 
     return (
         <Suspense fallback={<PostDetailSkeleton />}>
