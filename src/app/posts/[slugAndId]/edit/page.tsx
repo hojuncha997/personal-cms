3c00ef18c7512c70573c01979effda98923cf16f
hojuncha <hojun.cha997@gmail.com>
2025-03-05 20:15:24 +0900

    chore:  모든 파라미터를 프로미스로 변경

--- src/app/posts/[slugAndId]/edit/page.tsx
+++ src/app/posts/[slugAndId]/edit/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PostEditClient from './PostEditClient';
 
-export default async function Page({
-    params,
-    searchParams,
-}: {
-    params: { slugAndId: string };
-    searchParams: { [key: string]: string | string[] | undefined };
-}) {
-    const publicId = params.slugAndId.split('-').pop() || '';
+interface PageProps {
+    params: Promise<{
+        slugAndId: string;
+    }>;
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function Page({ params, searchParams }: PageProps) {
+    const [resolvedParams, resolvedSearchParams] = await Promise.all([params, searchParams]);
+    const publicId = resolvedParams.slugAndId.split('-').pop() || '';
     
     return <PostEditClient publicId={publicId} />;
-} 
+}
