caadc439fd10596418073704bf64716aef69d211
hojuncha <hojun.cha997@gmail.com>
2025-03-05 20:05:01 +0900

    chore: [slugAndId]의 params 타입을 Promise로 변경

--- src/app/posts/[slugAndId]/edit/page.tsx
+++ src/app/posts/[slugAndId]/edit/page.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import PostEditClient from './PostEditClient';
 
 interface PageProps {
-    params: {
+    params: Promise<{
         slugAndId: string;
-    };
+    }>;
     searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default function Page({ params }: PageProps) {
-    const publicId = params.slugAndId.split('-').pop() || '';
+export default async function Page({ params }: PageProps) {
+    const resolvedParams = await params;
+    const publicId = resolvedParams.slugAndId.split('-').pop() || '';
     
     return <PostEditClient publicId={publicId} />;
 } 
