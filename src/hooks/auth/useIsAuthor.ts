4119a76e1179ebfbc93c40b4e645699c6d4050a8
hojuncha997 <hojun.cha997@gmail.com>
2025-09-01 09:32:22 +0900

    feat: 요청 중복 제거 로직 추가

--- src/hooks/auth/useIsAuthor.ts
+++ src/hooks/auth/useIsAuthor.ts
@@ -1,7 +1,8 @@
 import { useAuthStore } from '@/store/useAuthStore';
 
 export const useIsAuthor = (authorUuid: string | undefined) => {
-    const { sub } = useAuthStore();
+    // Selector 최적화로 필요한 값만 구독
+    const sub = useAuthStore(state => state.sub);
     
     if (!sub || !authorUuid) {
         return false;
